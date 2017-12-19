45b48e4e8a71be3d986d7bcc6d245d9d5c4b8c7b
bweick <brian@biometrixtech.com>
2017-12-19 08:52:25 -0700

    Added 0x functions to operations object. Set up Web3 provider. Exported runServer fxn to eth_server to start js_server once eth_server is set up.

--- echo_server.js
+++ echo_server.js
@@ -1,32 +1,55 @@
+const ZeroEx = require('0x.js');
+const Web3 = require('web3');
+
 const net = require('net');
 
-const hostname = '127.0.0.1';
+// const hostname = process.argv[2];
+// const port = parseInt(process.argv[3]);
+
+const hostname = 'localhost';
 const port = 10000;
 
+const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+const zeroEx = new ZeroEx.ZeroEx(provider);
+
 var operations = function() {
-    this.add = function(x,y) {
-        return x+y;
+
+    this.salt = function(vars) {
+        return ZeroEx.ZeroEx.generatePseudoRandomSalt();
     };
 
-    this.mult = function(x,y) {
-        return x*y;
+    this.zrxAddress = async function(vars) {
+        try {
+            return await zeroEx.exchange.getZRXTokenAddressAsync();
+        } catch (err) {
+            console.log(err);
+        }
     };
+
+    this.testConnection = async function() {
+      return true
+    }
+
+    this.nullAddress = function(vars) {
+        return ZeroEx.ZeroEx.NULL_ADDRESS;
+    }
 };
 
 const handler = function(socket) {
-    socket.on('data', function(bytes) {
+    socket.on('data', async function(bytes) {
         msg = bytes.toString().replace(new RegExp("'", 'g'), "\"");
         msg = JSON.parse(msg);
 
         var opts =  new operations()
-        out = opts[msg.fxn](msg.x,msg.y)
-        
+        out = await opts[msg.fxn](msg.vars)
+
         return socket.write(out.toString());
         socket.write('end');
     });
 };
 
-
-const server = net.createServer(handler).listen(port, hostname, () => {
+const runServer = () => net.createServer(handler).listen(port, hostname, () => {
   console.log("Server running at http://"+hostname+":"+port.toString()+"/");
 });
+
+module.exports.runServer = runServer
